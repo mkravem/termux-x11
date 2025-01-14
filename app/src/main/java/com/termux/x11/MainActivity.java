669fd336b3321f0bc836980f6d66ff3156eb28e7
mkravem <157925454+mkravem@users.noreply.github.com>
2025-01-14 11:04:00 -0300

    Update MainActivity.java

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -198,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
             mInputHandler.handleHostSizeChanged(surfaceWidth, surfaceHeight);
             mInputHandler.handleClientSizeChanged(screenWidth, screenHeight);
             if (lorieView.getDisplay() == null || lorieView.getDisplay().getDisplayId() == Display.DEFAULT_DISPLAY)
-                name = "S6D7AT0B";
+                name = "eDP-0";
             else if (SamsungDexUtils.checkDeXEnabled(this))
                 name = "Dex Display";
             else
