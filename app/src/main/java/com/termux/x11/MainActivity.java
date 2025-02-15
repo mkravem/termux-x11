3c5789cd6357552b153c9aca4b6e73110f4804c9
mkravem <157925454+mkravem@users.noreply.github.com>
2025-02-15 00:10:26 -0300

    Update MainActivity.java

--- app/src/main/java/com/termux/x11/MainActivity.java
+++ app/src/main/java/com/termux/x11/MainActivity.java
@@ -198,11 +198,11 @@ public class MainActivity extends AppCompatActivity implements View.OnApplyWindo
             mInputHandler.handleHostSizeChanged(surfaceWidth, surfaceHeight);
             mInputHandler.handleClientSizeChanged(screenWidth, screenHeight);
             if (lorieView.getDisplay() == null || lorieView.getDisplay().getDisplayId() == Display.DEFAULT_DISPLAY)
-                name = "virtio-0";
+                name = "virglrenderer-0";
             else if (SamsungDexUtils.checkDeXEnabled(this))
-                name = "virtio-0";
+                name = "virglrenderer-0";
             else
-                name = "virtio-0";
+                name = "virglrenderer-0";
             LorieView.sendWindowChange(screenWidth, screenHeight, framerate, name);
 
             if (service != null && !LorieView.renderingInActivity()) {
